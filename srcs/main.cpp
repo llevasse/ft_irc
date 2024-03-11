2a9cacf68de52e5aceee9ff9f1cbca96b6b4cf2f
llevasse <leonardlevasseur@yahoo.com>
2024-03-11 10:44:22 +0100

    added try-catch to main && remove constructor mess

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,6 +1,17 @@
 #include "Irc.hpp"
 
 int main(int argc, char **argv){
-	if (argc == 3)
-		Irc server(argv[1], argv[2]);
+	if (argc == 3){
+		try {
+			Irc server(argv[1], argv[2]);
+			server.createUser(0);
+			server.createUser(1);
+			server.createUser(2);
+
+			server.getUser(1);
+		}
+		catch ( std::exception &e ){
+			std::cerr << "Error : " << e.what() << std::endl;
+		}
+	}
 }
