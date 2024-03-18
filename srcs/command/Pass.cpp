f85da102bb28c1921d3a516ea8b24c37af70070e
Leo <leonardlevasseur@yahoo.com>
2024-03-18 16:12:56 +0100

    PASS can send not enough argument

--- srcs/command/Pass.cpp
+++ srcs/command/Pass.cpp
@@ -3,6 +3,23 @@
 Pass::Pass( Server *server, Client *client, std::string param){
 	std::string	pass = param.substr(0, param.find_first_of(" \n\r\t"));
 	(void)server;
+	bool found	= 0;
+	std::string	reply = ":" + client->getNickname() + "!" + client->getUsername() + "@localhost 461 " + client->getNickname() +  " :Not enough parameters\r\n";
+	for (std::string::reverse_iterator rit=param.rbegin(); rit!=param.rend(); rit++){
+		std::cout << *rit;
+		if (!isspace(*rit)){
+			found = 1;
+			break ;
+		}
+		else if (rit == param.rend()){
+			client->sendData(reply);
+			return ;
+		}
+	}
+	if (!found){
+		client->sendData(reply);
+		return ;
+	}
 	client->setPass(pass);
 }
 
