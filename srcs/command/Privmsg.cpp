12c76d39dba1a3acb8d1e8787cf09505e21ccc25
Leo <leonardlevasseur@yahoo.com>
2024-03-19 17:25:06 +0100

    merge

--- srcs/command/Privmsg.cpp
+++ srcs/command/Privmsg.cpp
@@ -5,7 +5,7 @@ void Message::privmsg(){
 	std::string	name = _param.substr(0, del);
 	std::string mess = _param.substr(del + 1);
 
-	for (std::map< int, Client * >::iterator it = _server->getClientsMap().begin(); it != _server->getClientsMap().end(); it++){
+	for (std::map< int, Client * >::const_iterator it = _server->getClientsMap().begin(); it != _server->getClientsMap().end(); it++){
 		if (it->second->getNickname() == name){
 			it->second->sendData(mess);
 			return ;
