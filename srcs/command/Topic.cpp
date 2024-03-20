f7aa10c07248be277b1482504ec5e92289112727
Leo <leonardlevasseur@yahoo.com>
2024-03-20 16:36:17 +0100

    added reply if channel doesnt exist

--- srcs/command/Topic.cpp
+++ srcs/command/Topic.cpp
@@ -4,11 +4,18 @@
 //or		TOPIC #channel
 
 void Message::topic(){
+	std::string	reply = ":" + _client->getNickname() + "!" + _client->getUsername() + "@localhost 403 " + _client->getNickname() +  " :No such channel.";
 	std::map<std::string, Channel *> channels = _server->getChannels();
 	size_t		beg		= _param.find("#");
-	if (beg == std::string::npos)
+	if (beg == std::string::npos){
+		_client->sendData(reply);
 		return ;
+	}
 	size_t		nameDel = _param.find(" ");
 	std::string name = _param.substr(beg + 1, nameDel - beg);
+	if (channels.find(name) == channels.end()){
+		_client->sendData(reply);
+		return ;
+	}
 	channels[name]->topic(_client, _param.substr(nameDel + 1));
 }
